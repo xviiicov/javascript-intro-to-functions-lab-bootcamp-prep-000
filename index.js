fe7d353efa857eaed3ff37442c684561ae9c84b9
Malcolm Nesbeth <mnesbeth@protonmail.ch>
2019-11-20 19:30:31 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function logShout(string){
+  console.log('HELLO') 
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logWhisper(string) {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string){
+  console.log('HELLO!')
+}
+
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase
+
+var lowercase = 'hello!'
+
+lowercase.toLowerCase() === lowercase
+
+var mixedCase = 'Hi there!'
+
+mixedCase.toLowerCase() === mixedCase
+
+mixedCase.toUpperCase() === mixedCase
